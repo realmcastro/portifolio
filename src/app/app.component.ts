8e771280f2a2bbe99f174b7f3c7e0183a30e2e3d
Matheus <Matheuscastro070@gmail.com>
2024-08-21 13:50:03 -0300

    Primeiro Dia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { ProjectsComponent } from './components/pages/projects/projects.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
